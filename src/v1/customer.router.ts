779b3314d796c9647caa6f505286fa14de54d32b
Manuel Feller <github@devsvr.ws>
2022-08-21 19:20:38 +0200

    improving swagger for working endpoints

--- src/v1/customer.router.ts
+++ src/v1/customer.router.ts
@@ -6,12 +6,11 @@ export const customerRouter = express.Router();
 
 /**
  * @swagger
- * /v1/customer/filterby/FILTER/sortby/SORT:
+ * /v1/customer/filterby/{filer}/sortby/{sort}:
  *   get:
  *     summary: Get a filtered and sorted list of customers (all properties of the base object, but without notes)
  *     description:
  *       This endpoint provides a filtered and sorted list of all customers that are in the database
- *       <strong>Token as Authorization Bearer required</strong>
  *     tags:
  *       - customer
  */
@@ -28,12 +27,11 @@ customerRouter.get("/filterBy/:filter/sortBy/:sort", Authenticator.getTokenCheck
 
 /**
  * @swagger
- * /v1/customer/sortby/SORT/filterby/FILTER:
+ * /v1/customer/sortby/{sort}/filterby/{filter}:
  *   get:
  *     summary: Get a sorted and filtered list of customers (all properties of the base object, but without notes)
  *     description:
  *       This endpoint provides a sorted and filtered list of all customers that are in the database
- *       <strong>Token as Authorization Bearer required</strong>
  *     tags:
  *       - customer
  */
@@ -50,12 +48,11 @@ customerRouter.get("/sortBy/:sort/filterBy/:filter", Authenticator.getTokenCheck
 
 /**
  * @swagger
- * /v1/customer/filterby/FILTER:
+ * /v1/customer/filterby/{filter}:
  *   get:
  *     summary: Get a filtered list of customers (all properties of the base object, but without notes)
  *     description:
  *       This endpoint provides a filtered list of all customers that are in the database
- *       <strong>Token as Authorization Bearer required</strong>
  *     tags:
  *       - customer
  */
@@ -72,12 +69,11 @@ customerRouter.get("/filterBy/:filter", Authenticator.getTokenCheck(), async (re
 
 /**
  * @swagger
- * /v1/customer/sortby/SORT:
+ * /v1/customer/sortby/{sort}:
  *   get:
  *     summary: Get a sorted list of customers (all properties of the base object, but without notes)
  *     description:
  *       This endpoint provides a sorted list of all customers that are in the database
- *       <strong>Token as Authorization Bearer required</strong>
  *     tags:
  *       - customer
  */
@@ -95,20 +91,39 @@ customerRouter.get("/sortBy/:sort", Authenticator.getTokenCheck(), async (req: R
 
 /**
  * @swagger
- * /v1/customer/byId/ID:
+ * /v1/customer/byId/{id}:
  *   get:
  *     summary: Get all details of a customer
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: UUID of the user who's data should be retrieved.
+ *         schema:
+ *           type: string
  *     description:
  *       This endpoint provides all details of a customer, including all notes that are assigned
- *       <strong>Token as Authorization Bearer required</strong>
+ *     responses:
+ *       200:
+ *         description: A single user including the assigned notes.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CustomerWithNotes'
+ *       500:
+ *         description: Server side error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
  *     tags:
  *       - customer
  */
 customerRouter.get("/byId/:id", Authenticator.getTokenCheck(), async (req: Request, res: Response, next: NextFunction) => {
 	res.setHeader('content-type', 'application/json');
 	try {
-		const customerList = await DataStore.getInstance().getAllCustomerData(req.params['id']);
-		res.status(200).send(JSON.stringify(customerList));	
+		const customerData = await DataStore.getInstance().getAllCustomerData(req.params['id']);
+		res.status(200).send(JSON.stringify(customerData));	
 	} catch (err) {
 		console.log(err);
 		next(new HttpError(500, new Error('Error when accessing the Data Store')));
@@ -119,10 +134,24 @@ customerRouter.get("/byId/:id", Authenticator.getTokenCheck(), async (req: Reque
  * @swagger
  * /v1/customer/all:
  *   get:
- *     summary: Get a list of all customers (all properties of the base object, but without notes)
+ *     summary: Get a list of all customers (all properties of the customer object, but without notes)
  *     description:
  *       This endpoint provides a list of all customers that are in the database
- *       <strong>Token as Authorization Bearer required</strong>
+ *     responses:
+ *       200:
+ *         description: The list of customers.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Customer'
+ *       500:
+ *         description: Server side error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
  *     tags:
  *       - customer
  */
@@ -163,9 +192,29 @@ customerRouter.get("/all", Authenticator.getTokenCheck(), async (req: Request, r
  *                 example: current
  *     description:
  *       This endpoint allows updating the status of a customer. It can be "prospective", "current" or "non-active".
- *       <strong>Token as Authorization Bearer required</strong>
+ *     responses:
+ *       200:
+ *         description: An action result
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/ActionResult'
+ *       400:
+ *         description: Input parameter error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Server side error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
  *     tags:
- *       - notes
+ *       - customer
  */
 customerRouter.patch("/", Authenticator.getTokenCheck(), async (req: Request, res: Response, next: NextFunction) => {
 	res.setHeader('content-type', 'application/json');
