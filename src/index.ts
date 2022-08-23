a8bcbbbc6f31460cb6b9db939ca93c15a9b9ef46
Manuel Feller <github@devsvr.ws>
2022-08-23 14:32:04 +0200

    cleanup, adding status endpoint and start page

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,8 @@ import cors, { CorsOptions } from 'cors';
 import helmet from 'helmet';
 
 const config = Configurator.instance;
-const app = express();
-const v1Router = express.Router();
 
+// set up CORS options
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     if (!origin) { // no origin submitted, let it pass
@@ -29,21 +28,41 @@ const corsOptions: CorsOptions = {
   }
 }
 
+// set up the express app
+const app = express();
+const v1Router = express.Router();
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-
 app.use("/v1", v1Router);
 v1Router.use("/customer", customerRouter);
 v1Router.use("/notes", notesRouter);
 
+// the root directory
 app.get('/', (_req: Request, res: Response) => {
-	res.send('The server is serving...');
+	let apiDocsInfo = '';
+	if (config.publishSwagger) {
+		apiDocsInfo = `<p>
+		  You can access the API documentation at <a href="./api-docs/">this link</a> if you want to explore how it works.<br/>
+			The demo access token is <code>01234567890</code>.
+		</p>`;
+	}
+	res.setHeader('content-type', 'text/html');
+	res.send(`<!doctype html><html><head><title>Demo API</title><style>body { font-family: sans-serif; }</style></head><body>
+	<h1>Demo API</h1>
+	<p>This is the API server for the Demo API.</p>
+	<p>
+	  If a simple <code>200</code> is not enough and you do not want to check HTML content to check the status:<br />
+	  you can get	a JSON response for status checking at the <a href="./status/"><code>status</code></a> endpoint.
+	</p>
+	${apiDocsInfo}
+	</body></html>`);
 });
 
-app.get('/test/:filter', (request: Request, res: Response) => {
+// simple status endpoint
+app.get('/status', (_req: Request, res: Response) => {
 	res.setHeader('content-type', 'application/json');
-	res.send(JSON.stringify(request.params));
+	res.send(JSON.stringify({status: 'OK'}));
 });
 
 // handle undefined routes & errors that occurred / were raised
@@ -63,7 +82,6 @@ app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
 });
 
 // swagger API documentation
-
 if (config.publishSwagger) {
   const openApiOptions = {
     definition: {
@@ -103,13 +121,8 @@ if (config.publishSwagger) {
   };
 
   const specs = swaggerJsdoc(openApiOptions);
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs)
-  );
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 }
 
 // start listening
-
 app.listen(config.serverPort, () => console.log(`API Server: Server listening at port ${config.serverPort}...`))
